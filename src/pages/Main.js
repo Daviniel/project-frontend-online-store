95282b10d88d8f9600108b36008c0cac0994b19b
Daviniel Nascimento <eng.daviniel@gmail.com>
2024-02-26 12:20:56 -0300

    requerimento 2 concluido, adicionado a página  principal o campo de busca

---
+++ src/pages/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Main() {
+    return(
+        <p data-testid='home-initial-message' > Digite algum termo de pesquisa ou escolha uma categoria.</p>
+    );
+};
--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,13 @@
-const BASE_URL = 'https://api.mercadolibre.com';
-
 export async function getCategories() {
-  try {
-    const response = await fetch(`${BASE_URL}/sites/MLB/categories`);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching categories:', error);
-    throw error;
-  }
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  try {
-    const response = await fetch(`${BASE_URL}/sites/MLB/search?category=${categoryId}&q=${query}`);
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.error('Error fetching products:', error);
-    throw error;
-  }
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
