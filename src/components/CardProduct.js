28391bc967647298b1053f108ca898d89bb42742
Daviniel Nascimento <eng.daviniel@gmail.com>
2024-02-27 12:50:03 -0300

    adicionado o filtro pelas as categorias e nomes de cada item na api

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import FreeShipping from './FreeShipping';
 
 class CardProduct extends Component {
   constructor() {
