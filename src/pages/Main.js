eb28a87ea8457cda5ab8add6f0c380098784c52f
Daviniel Nascimento <eng.daviniel@gmail.com>
2024-02-27 11:11:49 -0300

    corrigindo bug

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Header from '../components/Header';
 
-export default function Main() {
-    return(
-        <><div>
-            <Header cartCount={cartCount} />
-            <p data-testid='home-initial-message'> Digite algum termo de pesquisa ou escolha uma categoria.</p>
-        </div><ul>
-                <span>Nenhum produto foi encontrado</span>
-            </ul></>
-    );
+const Main = ({ cartCount }) => {
+  return (
+    <>
+      <div>
+        <Header cartCount={cartCount} />
+        <p data-testid='home-initial-message'>
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+      <ul>
+        <span>Nenhum produto foi encontrado</span>
+      </ul>
+    </>
+  );
 };
+
+export default Main;
