cf1364b46d010b2414060a140aa40c29b755aec6
Daviniel Nascimento <eng.daviniel@gmail.com>
2024-02-27 11:35:13 -0300

    adicionado as categorias de buscas nas apis

---
+++ src/components/CardProduct.js
@@ -0,0 +1,83 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class CardProduct extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  handleCLick = ({ target }) => {
+    const { handleCart } = this.props;
+    let { value } = target;
+    let cart = localStorage.getItem('cartProducts');
+    cart = JSON.parse(cart);
+    value = JSON.parse(value);
+    if (!cart) {
+      localStorage.setItem('cartProducts', JSON.stringify([value]));
+    } else {
+      cart.push(value);
+      localStorage.setItem('cartProducts', JSON.stringify(cart));
+    }
+    handleCart();
+  }
+
+  render() {
+    const { list } = this.props;
+    const { shipping: { free_shipping: freeShipping } } = list;
+    return (
+      <div className="singleCardProduct">
+        <Link
+          to={ `/product/${list.id}` }
+          data-testid="product-detail-link"
+          className="cardProduct"
+        >
+          <li data-testid="product">
+            <img src={ list.thumbnail } alt={ list.title } />
+            <div className="productCardInfo">
+              <span>{ list.title }</span>
+              {/* <p>
+                R$
+                { String(list.price.toFixed(2)).replace('.', ',') }
+              </p> */}
+              <FreeShipping freeShipping={ freeShipping } price={ list.price } />
+            </div>
+          </li>
+        </Link>
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ this.handleCLick }
+          value={ JSON.stringify(list) }
+          className="addCartBtn"
+        >
+          Adicionar ao Carrinho
+        </button>
+        {/* {freeShipping
+          && (
+            <span data-testid="free-shipping">
+              Frete Grátis!!
+            </span>
+          )} */}
+      </div>
+    );
+  }
+}
+
+CardProduct.propTypes = {
+  handleCart: PropTypes.func.isRequired,
+  list: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    shipping: PropTypes.shape({
+      free_shipping: PropTypes.bool.isRequired,
+    }),
+  }).isRequired,
+};
+
+export default CardProduct;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,20 +1,128 @@
-import React from 'react';
+import React, { Component } from 'react';
+import CardProduct from '../components/CardProduct';
 import Header from '../components/Header';
+import {
+  getCategories,
+  getProductsFromCategory,
+  getProductsFromQuery,
+} from '../services/api';
 
-const Main = ({ cartCount }) => {
-  return (
-    <>
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          categoriesList: [],
+          queryInput: '',
+          productList: [],
+          containerHeight: '82vh',
+        };
+    }
+
+    async componentDidMount() {
+        const categoriesList = await getCategories();
+        this.setState({
+        categoriesList,
+        });
+    }
+
+    adjustContainerHeight = (productList) => {
+        const NUMBER_OF_PRODUCTS = 2;
+        const containerHeight = productList.length > NUMBER_OF_PRODUCTS ? '100%' : '82vh';
+        this.setState({ containerHeight });
+    }
+    
+    handleClick = async ({ target }) => {
+        const { id } = target;
+        const response = await getProductsFromCategory(id);
+        const productList = response.results;
+        this.setState({ productList });
+        this.adjustContainerHeight(productList);
+    }
+    
+    handleButton = async ({ target }) => {
+        const { value } = target;
+        const response = await getProductsFromQuery(value);
+        const productList = response.results;
+        this.setState({ productList });
+        this.adjustContainerHeight(productList);
+    }
+    
+    handleChange = ({ target }) => {
+        const { name, value } = target;
+        this.setState({ [name]: value });
+    }
+    
+    handleCart = () => {
+        const cartItems = localStorage.getItem('cartProducts');
+        const cartCount = JSON.parse(cartItems).length;
+        this.setState({ cartCount });
+        localStorage.setItem('quant', cartCount);
+    }
+
+  render() {
+    const { categoriesList, queryInput, productList, containerHeight, cartCount } = this.state;
+    return (
       <div>
-        <Header cartCount={cartCount} />
-        <p data-testid='home-initial-message'>
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <Header cartCount={ cartCount } />
+        <main className="main">
+        <aside className="categoriesList" style={{ overflowY: 'scroll', height: containerHeight }}>
+            <h3>Categorias</h3>
+            {
+              categoriesList.map(({ id, name }) => (
+                <div key={ id }>
+                  <button
+                    type="button"
+                    data-testid="category"
+                    id={ id }
+                    onClick={ this.handleClick }
+                  >
+                    {name}
+                  </button>
+                </div>
+              ))
+            }
+          </aside>
+          <section className="productShowroom">
+            <p
+              data-testid="home-initial-message"
+            >
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+            <label htmlFor="queryInput">
+              <input
+                id="queryInput"
+                data-testid="query-input"
+                name="queryInput"
+                onChange={ this.handleChange }
+                value={ queryInput }
+                type="text"
+              />
+              <button
+                data-testid="query-button"
+                type="button"
+                onClick={ this.handleButton }
+                value={ queryInput }
+              >
+                Pesquisar
+              </button>
+            </label>
+            <ul className={ productList.length === 0 ? 'noProduct' : 'productList' }>
+              {
+                productList.length ? productList.map((list) => (
+                  <CardProduct
+                    key={ list.id }
+                    list={ list }
+                    handleCart={ this.handleCart }
+                  />
+                ))
+                  : <span>Nenhum produto foi encontrado</span>
+              }
+            </ul>
+          </section>
+        </main>
       </div>
-      <ul>
-        <span>Nenhum produto foi encontrado</span>
-      </ul>
-    </>
-  );
-};
+    );
+  }
+}
 
 export default Main;
