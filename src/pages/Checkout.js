e055e5f28c48d10ad5530a0d23ff53983acb1a63
Daviniel Nascimento <eng.daviniel@gmail.com>
2024-03-04 12:33:15 -0300

    adicionado o método de pagamento

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -151,6 +151,36 @@ function Checkout() {
             />
           </label>
           
+          <label htmlFor="#">
+            <img src="https://logodownload.org/wp-content/uploads/2019/09/boleto-logo.png" alt="Boleto bancário." />
+            <input
+              type="radio"
+              name="payment"
+              value="boleto"
+              onChange=''
+            />
+          </label>
+
+          <label htmlFor="#">
+            <img src="https://icons-for-free.com/iconfiles/png/512/payment+online+transaction+payment+method+visa+icon-1320186278106432321.png" alt="Visa." />
+            <input
+              type="radio"
+              name="payment"
+              value="visa"
+            />
+            <img src="https://pt.seaicons.com/wp-content/uploads/2015/06/Master-Card-icon.png" alt="Mastercard." />
+            <input
+              type="radio"
+              name="payment"
+              value="masterCard"
+            />
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNVz95FWm_kkRqqIpLaghhI0RhW8IXQAEgngPXQwBIjkCyCVoZ1CMC8Kl-IyyKrroIB7Y&usqp=CAU" alt="Elo." />
+            <input
+              type="radio"
+              name="payment"
+              value="elo"
+            />
+          </label>
 
           <button type="button" onClick={handleBuy}>
             Comprar
